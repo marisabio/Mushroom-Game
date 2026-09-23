77e74e0577917ee4ab29492decdc5364b7dbb241
Mari Sabio <mari.msabio@posteo.net>
2026-09-23 16:53:20 -0300

    implementação de modelos e mapas

--- Assets/Scripts/Main Gameplay/GameplayAnimationController.cs
+++ Assets/Scripts/Main Gameplay/GameplayAnimationController.cs
@@ -39,6 +39,7 @@ public class GameplayAnimationController : MonoBehaviour
                 {
                     animator.SetBool("isWalking", false);
                 }
+
             }
         }
     }
